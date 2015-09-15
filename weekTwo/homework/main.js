94daac0b83150f9caa86231057dd2ef60dca3aa7
thea <tr@thearae.com>
2015-09-15 11:42:57 -0400

    Working off a JSON file now

--- weekTwo/homework/main.js
+++ weekTwo/homework/main.js
@@ -1,4 +1,51 @@
+$.getJSON("days.json", parselife);
+  var days;
+  var timeVal;
+  var dayVal;
+  var myActivity;
+  var myLocation;
+  var schedule;
+
+
 $(document).ready(function(){
-    var timeVal = $("#timeselector").val();
+
+  $("#timeSelector").change(function(){
+      timeVal = $(this).val()
       console.log(timeVal);
+      var myActivity = whatamidoing(dayVal, timeVal);
+    });
+  $("#daySelector").change(function(){
+      dayVal = $(this).val()
+      console.log(dayVal);
+    });
+  $("#selectors").change(function(){
+    var imgLoc = "images/"+myLocation+".png";
+    var imgAct = "images/"+myActivity+".png";
+    $("#locationImg").attr("src",imgLoc);
+    $("#activityImg").attr("src",imgAct);
+  });
 });
+
+function parselife(data){
+  days = data.days;
+  for(var i =0; i < days.length; i++){
+    console.log(days[i].day);
+    schedule = days[i].schedule;
+    for(var j =0; j< schedule.length; j++){
+      console.log(schedule[j].time);
+    }
+  }
+}
+function myDay(day, time){
+  for(i=0; i<days.length; i++){
+    if (days[i].day == day){
+      var schedule =days[i].schedule;
+      for(j=0; j<schedule.length; j++){
+        if( schedule[j].time == timeVal){
+           myActivity = schedule[j].activity;
+           myLocation = schedule[j].location;
+        }
+      }
+    }
+  }
+}
--- weekTwo/homework/test.js
+++ weekTwo/homework/test.js
@@ -1,11 +1,51 @@
-$.getJSON("theasLife.JSON", parselife);
+$.getJSON("days.json", parselife);
+  var days;
+  var timeVal;
+  var dayVal;
+  var myActivity;
+  var myLocation;
+  var schedule;
+
+
+$(document).ready(function(){
+
+  $("#timeSelector").change(function(){
+      timeVal = $(this).val()
+      console.log(timeVal);
+      var myActivity = whatamidoing(dayVal, timeVal);
+    });
+  $("#daySelector").change(function(){
+      dayVal = $(this).val()
+      console.log(dayVal);
+    });
+  $("#selectors").change(function(){
+    var imgLoc = "images/"+myLocation+".png";
+    var imgAct = "images/"+myActivity+".png";
+    $("#locationImg").attr("src",imgLoc);
+    $("#activityImg").attr("src",imgAct);
+  });
+});
 
 function parselife(data){
-  var life = data.theasLife;
-  for (var i = 0; i<life.length; i++){
-    var day = life[i].days;
-    var p = "<li>" + day + "</li>";
-    $ ("#dayTime").append(p);
+  days = data.days;
+  for(var i =0; i < days.length; i++){
+    console.log(days[i].day);
+    schedule = days[i].schedule;
+    for(var j =0; j< schedule.length; j++){
+      console.log(schedule[j].time);
+    }
   }
-  //console.log(data);
+}
+function myDay(day, time){
+  for(i=0; i<days.length; i++){
+    if (days[i].day == day){
+      var schedule =days[i].schedule;
+      for(j=0; j<schedule.length; j++){
+        if( schedule[j].time == timeVal){
+           myActivity = schedule[j].activity;
+           myLocation = schedule[j].location;
+        }
+      }
+    }
+  }
 }
